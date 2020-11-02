122fc132c776646bf9ec9c6ec373451073b9e038
Viniciusadm <viniciusam.22@gmail.com>
2020-11-02 06:31:36 -0300

    Página feita

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?PHP
 
-$dadosJSON = file_get_contents('https://www.googleapis.com/youtube/v3/videos?id=IHNzOHi8sJs&key=AIzaSyBRJ3773xt2m4h5WJjg4u_bgbSNua0psvk&fields=items(snippet(title,thumbnails(maxres(url))),statistics(viewCount))&part=snippet,statistics');
-$dadosJSONDecodificados = json_decode($dadosJSON);
+$playlistJSON = file_get_contents('https://www.googleapis.com/youtube/v3/playlistItems?playlistId=PLqSz0ak5p5kWs6ry0vo-aO2RRc1maiq2h&key=AIzaSyBRJ3773xt2m4h5WJjg4u_bgbSNua0psvk&fields=items(contentDetails(videoId))&part=contentDetails&maxResults=10');
+$playlistJSONDecodificada = json_decode($playlistJSON);
+
+$url1 = 'https://www.googleapis.com/youtube/v3/videos?id=';
+$url2 = '&key=AIzaSyBRJ3773xt2m4h5WJjg4u_bgbSNua0psvk&fields=items(snippet(title,thumbnails(standard(url))),statistics(viewCount))&part=snippet,statistics';
 
 ?>
 
@@ -16,7 +19,26 @@ $dadosJSONDecodificados = json_decode($dadosJSON);
 </head>
 <body>
 
-<img src="<?php echo $dadosJSONDecodificados->items[0]->snippet->thumbnails->maxres->url ?>" width="320" height="180">
+<?PHP
+
+foreach($playlistJSONDecodificada as $array) {
+    foreach($array as $item) {
+        $id = $item->contentDetails->videoId;
+        $video = file_get_contents($url1.$id.$url2);
+        $videoDecodificado = json_decode($video);
+        $titulo = $videoDecodificado->items[0]->snippet->title;
+        $visualizacoes = $videoDecodificado->items[0]->statistics->viewCount;
+        $img = $videoDecodificado->items[0]->snippet->thumbnails->standard->url;
+        
+        echo '<pre>';
+        echo '<img src="'.$img.'">';
+        echo '<br>';
+        echo $titulo;
+        echo '<br>';
+        echo $visualizacoes;
+    }
+}
+?>
 
 </body>
 </html>
