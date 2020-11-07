367c7bc10282427709be075a747bacb6b8451180
Viniciusadm <viniciusam.22@gmail.com>
2020-11-06 22:12:45 -0300

    Adiçao de uma porrada de coisas

---
+++ js/teste.js
@@ -0,0 +1,3 @@
+numero = "1.222.333"
+
+console.log(parseInt(numero))
--- js/visualizacoes.js
+++ js/visualizacoes.js
@@ -1,4 +1,4 @@
-const key = 'AIzaSyBJ77zlJ443xyt0aF0Opxq0PjIrz4P3jnoA'
+const key = 'AIzaSyBJ77lJ443xyt0aF0Opxq0PjIrz4P3jnoAa'
 const idPlaylist = 'PLqSz0ak5p5kWs6ry0vo-aO2RRc1maiq2h'
 
 const url1 = 'https://www.googleapis.com/youtube/v3/videos?id='
@@ -8,7 +8,6 @@ async function getContent() {
 
     const response = await fetch('https://www.googleapis.com/youtube/v3/playlistItems?playlistId=' + idPlaylist + '&key=' + key + '&fields=items(contentDetails(videoId))&part=contentDetails&maxResults=50')
     const playlist = await response.json()
-
     var conteudo = ''
     let videosIds = []
 
@@ -59,21 +58,22 @@ async function getContent() {
         let viewsLista = views.split('').reverse()
 
         numeroComPontos = []
-        
+
         viewsLista.forEach((valor, index) => {
-                if (index % 3 === 0 && index !== 0) {
-                    numeroComPontos.push('.', valor)
-                } else {
-                    numeroComPontos.push(valor)
-                }
-            })
-        
-        views = numeroComPontos.reverse().join('')
+            if (index % 3 === 0 && index !== 0) {
+                numeroComPontos.push('.', valor)
+            } else {
+                numeroComPontos.push(valor)
+            }
+        })
+
+        viewsNumero = numeroComPontos.reverse().join('')
 
         listaDeVideos.push({
             urlImagem,
             nomeVideo,
             views,
+            viewsNumero,
             id
         })
     }
@@ -84,72 +84,59 @@ async function getContent() {
     let numeroLinha2 = 3
 
     for (video of listaDeVideos) {
-        if (contador === 1){
+        if (contador === 1) {
             conteudo +=
-            `<div class="row">
+                `<div class="row">
             <div class="video p-2 col-12 col-sm-6 col-md-4">
             <a href="https://www.youtube.com/watch?v=${video.id}" target="_blank"><img class="img-fluid" src="${video.urlImagem}"></a>
             <h1 class="py-2 mt-2 mb-0 tituloMusicas"><span class="primeiro">${contador}°</span> ${video.nomeVideo}</h1>
-            <p class="lead"><span class="bordaVisualizacoes">${video.views}</span> visualizações</p>
+            <p class="lead"><span class="bordaVisualizacoes">${video.viewsNumero}</span> visualizações</p>
             </div>`
-            
+
             contador++
             numeroLinha += 3
         } else if (contador === 2) {
             conteudo +=
-            `<div class="video p-2 col-12 col-sm-6 col-md-4">
+                `<div class="video p-2 col-12 col-sm-6 col-md-4">
             <a href="https://www.youtube.com/watch?v=${video.id}" target="_blank"><img class="img-fluid" src="${video.urlImagem}"></a>
             <h1 class="py-2 mt-2 mb-0 tituloMusicas"><span class="segundo">${contador}°</span> ${video.nomeVideo}</h1>
-            <p class="lead"><span class="bordaVisualizacoes">${video.views}</span> visualizações</p>
+            <p class="lead"><span class="bordaVisualizacoes">${video.viewsNumero}</span> visualizações</p>
             </div>`
-            
+
             contador++
         } else if (contador === 3) {
             conteudo +=
-            `<div class="video p-2 col-12 col-sm-6 col-md-4">
+                `<div class="video p-2 col-12 col-sm-6 col-md-4">
             <a href="https://www.youtube.com/watch?v=${video.id}" target="_blank"><img class="img-fluid" src="${video.urlImagem}"></a>
             <h1 class="py-2 mt-2 mb-0 tituloMusicas"><span class="terceiro">${contador}°</span> ${video.nomeVideo}</h1>
-            <p class="lead"><span class="bordaVisualizacoes">${video.views}</span> visualizações</p>
+            <p class="lead"><span class="bordaVisualizacoes">${video.viewsNumero}</span> visualizações</p>
             </div>
             </div>`
-            
+
             contador++
             numeroLinha2 += 3
-        } else if (contador === numeroLinha) {
-            conteudo +=
-            `<div class="row">
-            <div class="video p-2 col-12 col-sm-6 col-md-4">
-            <a href="https://www.youtube.com/watch?v=${video.id}" target="_blank"><img class="img-fluid" src="${video.urlImagem}"></a>
-            <h1 class="py-2 mt-2 mb-0 tituloMusicas">${contador}° ${video.nomeVideo}</h1>
-            <p class="lead"><span class="bordaVisualizacoes">${video.views}</span> visualizações</p>
-            </div>`
-            
-            contador++
-            numeroLinha += 3
-        } else if (contador === numeroLinha2) {
-            conteudo +=
-                `<div class="video p-2 col-12 col-sm-6 col-md-4">
-                <a href="https://www.youtube.com/watch?v=${video.id}" target="_blank"><img class="img-fluid" src="${video.urlImagem}"></a>
-                <h1 class="py-2 mt-2 mb-0 tituloMusicas">${contador}° ${video.nomeVideo}</h1>
-                <p class="lead"><span class="bordaVisualizacoes">${video.views}</span> visualizações</p>
-                </div>
-                </div>`
-
-                contador++
-                numeroLinha2 += 3
         } else {
-                conteudo +=
+            if (contador === numeroLinha) {
+                conteudo += '<div class="row">'
+                numeroLinha += 3
+            }
+
+            conteudo +=
                 `<div class="video p-2 col-12 col-sm-6 col-md-4">
                 <a href="https://www.youtube.com/watch?v=${video.id}" target="_blank"><img class="img-fluid" src="${video.urlImagem}"></a>
                 <h1 class="py-2 mt-2 mb-0 tituloMusicas">${contador}° ${video.nomeVideo}</h1>
-                <p class="lead"><span class="bordaVisualizacoes">${video.views}</span> visualizações</p>
+                <p class="lead"><span class="bordaVisualizacoes">${video.viewsNumero}</span> visualizações</p>
                 </div>`
 
-                contador++
+            if (contador === numeroLinha2) {
+                conteudo += '</div>'
+                numeroLinha2 += 3
+            }
+            contador++
         }
     }
-
-        document.querySelector('main').innerHTML = conteudo
+    document.querySelector('main').innerHTML = conteudo
 }
+
 
 getContent()
